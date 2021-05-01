67a44b7a2d881e017425a418ffcf15c66014deca
Girijeshwar <girijeshwar15795@gmail.com>
2021-05-01 06:05:50 +0530

    Initail comment

---
+++ blog/singleFunc.py
@@ -0,0 +1,6 @@
+from chat import jarvischat
+
+
+def letschat(sent):
+    res = jarvischat(sent)
+    return res
---
+++ blog/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+from .views import (
+    PostListView, 
+    PostDetailView, 
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+
+#i will add chatbot here in future
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-details'),
+    path('post/new/', PostCreateView.as_view(), name='post-Create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
+    path('chatbot/', views.Letschat, name='chat-bot'),
+]
